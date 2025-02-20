4bb5f234d8c0fd97958b7bb7d53579376ce582b7
Aleksandr Stepanov <visp.asap@gmail.com>
2025-02-20 22:47:17 +0600

    Extracted JSON data out of script tag using unbelievable kostyl

---
+++ Requests.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Net.Security;
+
+namespace bdu_parser
+{
+    class Requests
+    {
+        private HttpClient httpClient;
+
+        public Requests(string url)
+        {
+            //Bypass certificate check
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            httpClient = new HttpClient(clientHandler)
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public async Task<string> GetBdu(string bduId)
+        {
+            try
+            {
+                using HttpResponseMessage responce = await httpClient.GetAsync(httpClient.BaseAddress + bduId);
+                responce.EnsureSuccessStatusCode();
+                string responceBody = await responce.Content.ReadAsStringAsync();
+
+                return responceBody;
+                
+            }
+            catch (HttpRequestException e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
