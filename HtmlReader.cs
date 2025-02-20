b3007a664d794a2d740e23df98a0c476659070d1
Aleksandr Stepanov <visp.asap@gmail.com>
2025-02-20 22:46:19 +0600

    Created the html reader

---
+++ HtmlReader.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+
+namespace bdu_parser
+{
+    class HtmlReader
+    {
+        public string Path { get; set; }
+
+        public HtmlReader(string path)
+        {
+            Path = path;
+        }
+
+        public List<string> ReadFromFile()
+        {
+            List<string> outputBdus = new List<string>();
+
+            var doc = new HtmlDocument();
+            doc.Load(Path);
+
+            var bduIdNodes = doc.DocumentNode.SelectNodes("//td[contains(@class, 'bdu')]");
+
+            foreach (var item in bduIdNodes)
+            {
+                outputBdus.Add(item.InnerText);
+            }
+
+            return outputBdus;
+        }
+
+        public void ReadFromString(string input)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(input);
+
+            var cweNodes = doc.DocumentNode.SelectNodes("//script[contains(., 'const v_model = reactive')]");
+
+            try
+            {
+                string cweJson = cweNodes[0].InnerText.Split("reactive")[2];
+
+                cweJson = cweJson.Remove(cweJson.LastIndexOf("});"));
+                cweJson = cweJson.Remove(0, 1);
+                cweJson = cweJson.Insert(cweJson.Length, "}");
+
+                Console.WriteLine(cweJson);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+                Console.WriteLine($"StackTrace: {e.StackTrace}");
+            }
+        }
+    }
+}
