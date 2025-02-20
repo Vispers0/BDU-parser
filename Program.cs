e07b333917affb0623e42b5d239aef0a57504564
Aleksandr Stepanov <visp.asap@gmail.com>
2025-02-20 22:45:17 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace bdu_parser
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string path = "C:\\Sharaga\\ZIvKS\\LR2\\vulnerabilities.html";
+
+            HtmlReader htmlReader = new HtmlReader(path);
+
+            List<string> bdus = htmlReader.ReadFromFile();
+
+            Requests requests = new Requests("https://bdu.fstec.ru/vul/");
+            string responce  = await requests.GetBdu("2025-01829");
+
+            htmlReader.ReadFromString(responce);
+        }
+    }
+}
